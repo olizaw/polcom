f6b4fec92802a74eae8daa1c2b16d84ff1a44f1d
ejyoon <ejyoon@stanford.edu>
2016-11-11 15:46:12 -0800

    coded increase in trial num
    to do: include text

--- design/study2/code/polcon.js
+++ design/study2/code/polcon.js
@@ -1,20 +1,31 @@
-var allimages = ["test1_background.png", "test1_listener1.png"]
+var allImages = ["test1_listener1", "test1_listener2", "test1_speaker1", "test1_speaker2"]
 var images = new Array();
-for (i = 0; i < allimages.length; i++) {
+for (i = 0; i < allImages.length; i++) {
     images[i] = new Image();
-    images[i].src = "figs/" + allimages[i];
+    images[i].src = "figs/" + allImages[i] + ".png";
+}
+
+var listenerList = [];
+var speakerList = [];
+for (i = 1; i < 13; i++) {
+    for (j = 1; j < 3; j++) {
+        listenerList.push("test" + i + "_listener" + j);
+        speakerList.push("test" + i + "_speaker" + j);
+    }
 }
 
 //******for handling sound; see helper function playPrompt(word)
 var audioSprite = $("#sound_player")[0];
 var handler;
 
-
 showSlide("instructions");
 
 
 var experiment = {
+    trial_num: 0,
     within_trial: 0,
+    listenerList: listenerList,
+    speakerList: speakerList,
 
     checkInput: function () {
         //subject ID
@@ -27,11 +38,10 @@ var experiment = {
     },
 
     next: function () {
-        console.log(experiment.within_trial)
-        experiment.listener1 = "test1_listener1";
-        experiment.listener2 = "test1_listener2";
-        experiment.speaker1 = "test1_speaker1";
-        experiment.speaker2 = "test1_speaker2";
+        experiment.listener1 = listenerList[2*experiment.trial_num];
+        experiment.listener2 = listenerList[2*experiment.trial_num+1];
+        experiment.speaker1 = speakerList[2*experiment.trial_num];
+        experiment.speaker2 = speakerList[2*experiment.trial_num+1];
 
         leftname = "figs/" + experiment.speaker1 + ".png";
         centername = "figs/" + experiment.listener1 + ".png";
@@ -41,7 +51,6 @@ var experiment = {
         if (experiment.within_trial == 0) {
             var characters_html = "";
             characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
-
             $("#characters").html(characters_html);
             experiment.within_trial++;
 
@@ -49,14 +58,12 @@ var experiment = {
             var characters_html = "";
             characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
             characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
-
             $("#characters").html(characters_html);
             experiment.within_trial++;
 
             $('#leftPic').bind('click touchstart', function (event) {
                 playPrompt("apple_pos");
             });
-
 
         } else if (experiment.within_trial == 2) {
             var characters_html = "";
@@ -71,29 +78,23 @@ var experiment = {
             });
 
         } else if (experiment.within_trial == 3) {
-            //            var characters_html = "";
-            //            characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
-            //            characters_html += '<div><img class="pic" src="' + centername2 + '"alt="' + centername2 + '" id= "centerPic"/></div>'
-            //            characters_html += '<div><img class="pic" src="' + rightname + '"alt="' + rightname + '" id= "rightPic"/></div>'
             $("#centerPic").fadeOut("slow");
 
             $("#characters").html(characters_html);
             experiment.within_trial = 0;
-            // trialNum++
+
+            $('#leftPic2').bind('click touchstart', function (event) {
+                playPrompt("apple_pos");
+            });
+            $('#rightPic').bind('click touchstart', function (event) {
+                playPrompt("apple_neg");
+            });
+            experiment.trial_num++;
 
         }
 
         showSlide("stage");
         bgChange("url('figs/practice1_background.png')");
-
-    },
-
-    next2: function () {
-        //        function changepic() {
-        //            document.getElementById("leftPic").style.top = 2000 + "px";
-        //        }
-        //        changepic();
-        $("#centerPic").fadeOut("slow");
 
     }
 }
