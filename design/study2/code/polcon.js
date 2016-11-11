60847879d4563ee997f18afe238f7c9f5af6c6a3
ejyoon <ejyoon@stanford.edu>
2016-11-11 12:55:03 -0800

    trial mockup
    to do:
    - voice over when click
    - then try two trials

--- design/study2/code/polcon.js
+++ design/study2/code/polcon.js
@@ -7,7 +7,10 @@ for (i = 0; i < allimages.length; i++) {
 
 showSlide("instructions");
 
+
 var experiment = {
+    within_trial: 0,
+    
     checkInput: function () {
         //subject ID
         if (document.getElementById("subjectID").value.length < 1) {
@@ -19,6 +22,7 @@ var experiment = {
     },
 
     next: function () {
+        console.log(experiment.within_trial)
         experiment.listener1 = "test1_listener1";
         experiment.listener2 = "test1_listener2";
         experiment.speaker1 = "test1_speaker1";
@@ -26,14 +30,45 @@ var experiment = {
 
         leftname = "figs/" + experiment.speaker1 + ".png";
         centername = "figs/" + experiment.listener1 + ".png";
+        centername2 = "figs/" + experiment.listener2 + ".png";
         rightname = "figs/" + experiment.speaker2 + ".png";
-        
-        var characters_html = "";
-        characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
-        characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
-        characters_html += '<div><img class="pic" src="' + rightname + '"alt="' + rightname + '" id= "rightPic"/></div>'
 
-        $("#characters").html(characters_html);
+        if (experiment.within_trial == 0) {
+            var characters_html = "";
+            characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
+
+            $("#characters").html(characters_html);
+            experiment.within_trial++;
+
+        } else if (experiment.within_trial == 1) {
+            var characters_html = "";
+            characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
+            characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
+
+            $("#characters").html(characters_html);
+            experiment.within_trial++;
+
+        } else if (experiment.within_trial == 2) {
+            var characters_html = "";
+            characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic2"/></div>'
+            characters_html += '<div><img class="pic" src="' + centername2 + '"alt="' + centername2 + '" id= "centerPic"/></div>'
+            characters_html += '<div><img class="pic" src="' + rightname + '"alt="' + rightname + '" id= "rightPic"/></div>'
+
+            $("#characters").html(characters_html);
+            experiment.within_trial++;
+
+        } else if (experiment.within_trial == 3) {
+//            var characters_html = "";
+//            characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
+//            characters_html += '<div><img class="pic" src="' + centername2 + '"alt="' + centername2 + '" id= "centerPic"/></div>'
+//            characters_html += '<div><img class="pic" src="' + rightname + '"alt="' + rightname + '" id= "rightPic"/></div>'
+            $("#centerPic").fadeOut("slow");
+
+            $("#characters").html(characters_html);
+            experiment.within_trial = 0;
+            // trialNum++
+
+        }
 
         showSlide("stage");
         bgChange("url('figs/practice1_background.png')");
@@ -41,11 +76,11 @@ var experiment = {
     },
 
     next2: function () {
-//        function changepic() {
-//            document.getElementById("leftPic").style.top = 2000 + "px";
-//        }
-//        changepic();
-         $("#centerPic").fadeOut("slow");
+        //        function changepic() {
+        //            document.getElementById("leftPic").style.top = 2000 + "px";
+        //        }
+        //        changepic();
+        $("#centerPic").fadeOut("slow");
 
     }
 }
