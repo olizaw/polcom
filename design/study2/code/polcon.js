cc92b2db4d78c0df886c7a78291e96d4fdea975a
ejyoon <ejyoon@stanford.edu>
2016-11-14 13:53:44 -0800

    included questions in the script
    to do:
    - change background
    - allow clicking the character to go to the next trial - recording the
    answer (click)

--- design/study2/code/polcon.js
+++ design/study2/code/polcon.js
@@ -21,7 +21,7 @@ var page2_script = ["Greg doesn’t like bright lights. Let's hear what he said!
 var page3_script = ["Bob also doesn’t like bright lights. Let's hear what he said!", "Suzy also wanted more water in her cup. Let's hear what she said!"];
 var page4_script = ["Do you remember what Greg and Bob said? Let's hear them one more time!", "Do you remember what Jamie and Suzy said? Let's hear them one more time!"];
 var page5_script = ["Which one was being more polite?", "Which one was being nicer?"];
-var page5_script = ["Which one would you rather play with?", "Which one will Fred give water to?"];
+var page6_script = ["Which one would you rather play with?", "Which one will Fred give water to?"];
 
 
 //******for handling sound; see helper function playPrompt(word)
@@ -29,7 +29,7 @@ var audioSprite = $("#sound_player")[0];
 var handler;
 
 //amount of white space between trials
-var normalPause = 0; 
+var normalPause = 0;
 
 showSlide("instructions");
 
@@ -62,72 +62,72 @@ var experiment = {
         centername2 = "figs/" + experiment.listener2 + ".png";
         rightname = "figs/" + experiment.speaker2 + ".png";
 
-        if (experiment.trial_num == experiment.total_trial_num) { 
+        if (experiment.trial_num == experiment.total_trial_num) {
             experiment.end();
         } else {
 
-        if (experiment.within_trial == 0) {
-            var characters_html = "";
-            characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
-            $("#characters").html(characters_html);
-            var prompt_html = page1_script[experiment.trial_num]
-            $("#prompt").html(prompt_html);            
-            experiment.within_trial++;
+            if (experiment.within_trial == 0) {
+                var characters_html = "";
+                characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
+                var prompt_html = page1_script[experiment.trial_num]
+                experiment.within_trial++;
 
-        } else if (experiment.within_trial == 1) {
-            var characters_html = "";
-            characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
-            characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
-            $("#characters").html(characters_html);
-            var prompt_html = page2_script[experiment.trial_num]
-            $("#prompt").html(prompt_html);            
-            experiment.within_trial++;
+            } else if (experiment.within_trial == 1) {
+                var characters_html = "";
+                characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic"/></div>'
+                characters_html += '<div><img class="pic" src="' + centername + '"alt="' + centername + '" id= "centerPic"/></div>'
+                var prompt_html = page2_script[experiment.trial_num]
+                experiment.within_trial++;
 
-            $('#leftPic').bind('click touchstart', function (event) {
-                playPrompt("apple_pos"); //fixme
-            });
+                $('#leftPic').bind('click touchstart', function (event) {
+                    playPrompt("apple_pos"); //fixme
+                });
 
-        } else if (experiment.within_trial == 2) {
-            var characters_html = "";
-            characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic2"/></div>'
-            characters_html += '<div><img class="pic" src="' + centername2 + '"alt="' + centername2 + '" id= "centerPic"/></div>'
-            characters_html += '<div><img class="pic" src="' + rightname + '"alt="' + rightname + '" id= "rightPic"/></div>'
-            $("#characters").html(characters_html);
-            var prompt_html = page3_script[experiment.trial_num]
-            $("#prompt").html(prompt_html);            
-            experiment.within_trial++;
-            $('#rightPic').bind('click touchstart', function (event) {
-                playPrompt("apple_neg"); //fixme
-            });
+            } else if (experiment.within_trial == 2) {
+                var characters_html = "";
+                characters_html += '<div><img class="pic" src="' + leftname + '"alt="' + leftname + '" id= "leftPic2"/></div>'
+                characters_html += '<div><img class="pic" src="' + centername2 + '"alt="' + centername2 + '" id= "centerPic"/></div>'
+                characters_html += '<div><img class="pic" src="' + rightname + '"alt="' + rightname + '" id= "rightPic"/></div>'
+                var prompt_html = page3_script[experiment.trial_num]
+                experiment.within_trial++;
+                $('#rightPic').bind('click touchstart', function (event) {
+                    playPrompt("apple_neg"); //fixme
+                });
 
-        } else if (experiment.within_trial == 3) {
-            $("#centerPic").fadeOut("slow");
+            } else if (experiment.within_trial == 3) {
+                $("#centerPic").fadeOut("slow");
+                $("#characters").html(characters_html);
+                $('#leftPic2').bind('click touchstart', function (event) {
+                    playPrompt("apple_pos"); //fixme
+                });
+                $('#rightPic').bind('click touchstart', function (event) {
+                    playPrompt("apple_neg"); //fixme
+                });
+                var prompt_html = page4_script[experiment.trial_num]
+                $("#prompt").html(prompt_html);
+                experiment.within_trial++;
+            } else if (experiment.within_trial == 4) {
+                var prompt_html = page5_script[experiment.trial_num]
+                experiment.within_trial++;
+            } else if (experiment.within_trial == 5) {
+                var prompt_html = page6_script[experiment.trial_num]
+                experiment.trial_num++;
+                experiment.within_trial = 0;
+            }
 
-            $("#characters").html(characters_html);
-            experiment.within_trial = 0;
-
-            $('#leftPic2').bind('click touchstart', function (event) {
-                playPrompt("apple_pos"); //fixme
-            });
-            $('#rightPic').bind('click touchstart', function (event) {
-                playPrompt("apple_neg"); //fixme
-            });
-            var prompt_html = page4_script[experiment.trial_num]
-            $("#prompt").html(prompt_html);            
-            experiment.trial_num++;
+            showSlide("stage");
+            bgChange("url('figs/practice1_background.png')");
         }
-
-        showSlide("stage");
-        bgChange("url('figs/practice1_background.png')");
-        }
+        $("#prompt").html(prompt_html);
+        $("#characters").html(characters_html);
 
     },
-    
-    end: function() {
-    		setTimeout(function() {
-			$("#stage").fadeOut();
-		}, normalPause);
-		showSlide("finish");
-		document.body.style.background = "black";
-}
+
+    end: function () {
+        setTimeout(function () {
+            $("#stage").fadeOut();
+        }, normalPause);
+        showSlide("finish");
+        document.body.style.background = "black";
+    }
 }
