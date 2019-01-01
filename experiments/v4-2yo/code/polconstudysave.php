71a52cdeed2d8b09e180abb7fa4f78baa957bd67
Erica Yoon <ejyoon@stanford.edu>
2018-12-31 16:34:12 -0800

    began cogsci2019 draft

--- experiments/v4-2yo/code/polconstudysave.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$result_string = $_POST['postresult_string'];
-	file_put_contents('polcon_results_' . substr($result_string, 0, strpos($result_string, ",")) . ".csv", $result_string, FILE_APPEND);
-?>
